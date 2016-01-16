3c6bddad7ba94214e427b42768b3fd62dc5b35a9
gbadi <gbadi@mailjet.com>
2016-01-16 13:03:30 +0100

    clean

--- test/verbal-exprejon/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns verbal-exprejon.core-test
-  (:require [clojure.test :refer :all]
-            [verbal-exprejon.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
